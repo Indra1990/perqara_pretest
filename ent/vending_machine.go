a5dce045fd1f3811fbb1bd2e608cef47aafcec06
indra1990 <muhammad.indra65@gmail.com>
2024-05-25 16:54:19 +0700

    add git ignore

--- ent/vending_machine.go
+++ ent/vending_machine.go
@@ -1,7 +1,6 @@
 package ent
 
 import (
-	"context"
 	"time"
 
 	"github.com/uptrace/bun"
@@ -15,44 +14,4 @@ type VendingMachines struct {
 	CreatedAt     time.Time `bun:",nullzero,default:current_timestamp"`
 	UpdatedAt     bun.NullTime
 	DeletedAt     time.Time `bun:",soft_delete,nullzero"`
-}
-
-var _ bun.AfterCreateTableHook = (*VendingMachines)(nil)
-
-func (vendingMachine *VendingMachines) AfterCreateTable(ctx context.Context, query *bun.CreateTableQuery) error {
-	// insertData := []map[string]interface{}{
-	// 	{
-	// 		"item":  "hjghfgh",
-	// 		"price": 909,
-	// 	},
-	// 	{
-	// 		"item":  "fhgfhgf",
-	// 		"price": 909,
-	// 	},
-	// 	{
-	// 		"item":  "fghgfh",
-	// 		"price": 909,
-	// 	},
-	// 	{
-	// 		"item":  "fghfhfgh",
-	// 		"price": 909,
-	// 	},
-	// 	{
-	// 		"item":  "gfgfhfdhfd",
-	// 		"price": 909,
-	// 	},
-	// }
-
-	sdfdsf := []VendingMachines{
-		VendingMachines{
-			Item:  "dsfdsf",
-			Price: 45454,
-		},
-	}
-
-	_, err := query.NewInsert().Model(sdfdsf).Exec(ctx)
-	if err != nil {
-		return err
-	}
-	return nil
 }
